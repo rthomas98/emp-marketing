cd9464ab99ab28fe113239bb9b95dc7b3112567e
Rob Thomas <robt84@gmail.com>
2024-01-21 18:21:45 -0600

    configured tailwind

--- themes/empuls3/functions.php
+++ themes/empuls3/functions.php
@@ -141,7 +141,9 @@ function empuls3_scripts() {
 	wp_enqueue_style( 'empuls3-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'empuls3-style', 'rtl', 'replace' );
 
-	wp_enqueue_script( 'empuls3-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+    wp_enqueue_style( 'output', get_template_directory_uri() . '/dist/output.css', array() );
+
+    wp_enqueue_script( 'empuls3-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
