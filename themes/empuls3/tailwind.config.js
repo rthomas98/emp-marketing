691ad37092685ab6e349766cb3120f8f3a1ba446
Rob Thomas <robt84@gmail.com>
2024-01-27 10:18:54 -0600

    Update tailwind.config.js

--- themes/empuls3/tailwind.config.js
+++ themes/empuls3/tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./template-parts/*.{php,html,js}","./*.{php,html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "em-pink": "#BD1550",
+        "em-purple": "#231B53",
+        "em-yellow": "#FFC259",
+        "em-dark-purple": "#1F1946",
+        "em-light": "#F8F9FA",
+      },
+    },
   },
   plugins: [],
 }
